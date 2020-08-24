e6020a0ebcd0abef4d3561e87cffc532ee9525d2
Ben <ashitman1312@gmail.com>
2020-08-24 22:05:05 +0800

    0824

--- gulpfile.js
+++ gulpfile.js
@@ -72,6 +72,7 @@ gulp.task('styles',function(){
 					sourceRoot: 'gulp/sass'
 			})) //sourceMap寫入在一樣的地方  
 			.pipe(gulp.dest('dist/css/ec030.so-buy.com/ezfiles/814/1814/img/862/')) //翻譯後的路徑
+			.pipe(gulp.dest('dist/css/www.uwell.com.tw/ezfiles/814/1814/img/862/')) //翻譯後的路徑
 			.pipe(gulp.dest('dist/css/')) //翻譯後的路徑
 			.pipe(connect.reload())
 })
