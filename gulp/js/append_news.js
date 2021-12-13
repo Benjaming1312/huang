309cccabbfc419e1630a8a84a5e80fde0c5e0aec
Ben <ashitman1312@gmail.com>
2021-12-13 19:04:12 +0800

    1213

--- gulp/js/append_news.js
+++ gulp/js/append_news.js
@@ -6,4 +6,9 @@ module.exports = function () {
   }
 
   $('.news_title').prependTo($('#pageptlist'))
+
+  $('#pageptlist .d-item').click(function () {
+    const url = $(this).find('a').attr('href')
+    window.location.href = url
+  })
 }
